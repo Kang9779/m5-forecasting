3075e7122e5dc3b75d40d776bcf0d01f2d37fffc
kangzhang <kangzhang_seu@163.com>
2020-03-25 19:35:42 +0800

    explore data analysis

---
+++ eda.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# file = './data/'
+
+# sales_train_validation = pd.read_csv(file+'sales_train_validation.csv')
+# # calendar = pd.read_csv(file+'calendar.csv')
+# # sell_price = pd.read_csv(file+'sell_prices.csv')
+
+
+# demand = sales_train_validation.iloc[1,-1913:]
+# print(len(demand))
+# plt.plot(demand)
+
